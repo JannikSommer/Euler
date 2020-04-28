7c0289cb6a77b1901ced2b1493442cf20356d2e5
Magnus Mølgaard Lund <themlund98@gmail.com>
2020-04-28 10:11:16 +0200

    Update StatementNode.java

--- ANTLR/EulerLanguage/src/AST/StatementNode.java
+++ ANTLR/EulerLanguage/src/AST/StatementNode.java
@@ -1,9 +1,6 @@
 package AST;
 
 public abstract class StatementNode extends ASTNode {
-    public boolean isReachable;
-    public boolean terminatesNormally;
-
     public StatementNode(ASTNode parentNode) {
         super(parentNode);
     }
