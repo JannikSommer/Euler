dff210dba15570f1edde2ccc6a4a8b7de4c64845
Magnus Mølgaard Lund <themlund98@gmail.com>
2020-05-01 13:57:17 +0200

    Declaration fix

--- ANTLR/EulerLanguage/src/symbolTable/typeDescriptors/NumberTypeDescriptor.java
+++ ANTLR/EulerLanguage/src/symbolTable/typeDescriptors/NumberTypeDescriptor.java
@@ -8,21 +8,19 @@ public class NumberTypeDescriptor extends TypeDescriptor {
 
     @Override
     public boolean canCalculate(TypeDescriptor type, String operator) {
-        if(isAssignable(type)) {
-            if(type.kind == TypeDescriptorKind.number) {
-                return true;
-            } else if(type.kind == TypeDescriptorKind.matrix) {
-                return operator.equals("+") || operator.equals("-") || operator.equals("*");
-            } else if(type.kind == TypeDescriptorKind.vector) {
-                return operator.equals("+") || operator.equals("-") || operator.equals("*");
-            }
+        if(type.kind == TypeDescriptorKind.number) {
+            return true;
+        } else if(type.kind == TypeDescriptorKind.matrix) {
+            return operator.equals("+") || operator.equals("-") || operator.equals("*");
+        } else if(type.kind == TypeDescriptorKind.vector) {
+            return operator.equals("+") || operator.equals("-") || operator.equals("*");
         }
         return false;
     }
 
     @Override
     public TypeDescriptor getResultType(TypeDescriptor type, String operator) {
-        if(isAssignable(type)) {
+        if(canCalculate(type, operator)) {
             return type;
         }
         return null;
--- ANTLR/EulerLanguage/src/visitors/semantics/TopDeclVisitor.java
+++ ANTLR/EulerLanguage/src/visitors/semantics/TopDeclVisitor.java
@@ -14,29 +14,37 @@ public class TopDeclVisitor extends SemanticsVisitor {
 
     @Override
     public void visit(DeclarationNode node) {
-        // Check if initialization value is the right type
+        IdentificationNode id = (IdentificationNode)node.children.get(0);
         ASTNode expression = node.children.get(1);
+
+        // Do Semantic-analysis on expression
+        expression.accept(this);
+
+        // Set declaration type to expression-node type
+        node.type = expression.type; // TODO: Do some type checking with the id node
+
+        // Check if initialization value is the right type
         if(expression != null) {
             expression.accept(new SemanticsVisitor(symbolTable));
             if(expression.type.kind == TypeDescriptorKind.error) {
                 //node.type = expression.type;
-            } else if(node.type.kind != expression.type.kind) {
+            } else if(!node.type.isAssignable(expression.type)) {
                 node.type = new ErrorTypeDescriptor("at line " + node.lineNumber + ":" + node.charPosition + ", " +
-                        " initialization for "+ "'" + ((IdentificationNode)node.children.get(0)).name + "'" + " value is wrong type.", node);
+                        "initialization for "+ "'" + id.name + "'" + " value is wrong type.", node);
             }
         }
 
         // Check if variable is already declared in symbol table. Otherwise add it.
-        if(symbolTable.declaredLocally(((IdentificationNode)node.children.get(0)).name)) {
+        if(symbolTable.declaredLocally(id.name)) {
             node.type = new ErrorTypeDescriptor("at line " + node.lineNumber + ":" + node.charPosition + ", " +
-                    " variable "+ "'" + ((IdentificationNode)node.children.get(0)).name + "'" + " already declared.", node);
-            ((IdentificationNode)node.children.get(0)).attributesRef =  null;
+                    " variable "+ "'" + id.name + "'" + " already declared.", node);
+            id.attributesRef =  null;
         } else {
             VariableAttributes attr = new VariableAttributes();
             attr.kind = AttributeKind.variableAttributes;                                       // What type
             attr.variableType = node.type;                                                      // Set type in attributes
-            ((IdentificationNode)node.children.get(0)).attributesRef = attr;                    // Link attributes to node
-            symbolTable.enterSymbol(((IdentificationNode)node.children.get(0)).name, attr);     // Define in symbol table
+            id.attributesRef = attr;                                                            // Link attributes to node
+            symbolTable.enterSymbol(id.name, attr);                                             // Define in symbol table
         }
     }
 }
